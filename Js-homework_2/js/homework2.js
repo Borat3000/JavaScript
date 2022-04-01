5227462444f2a0ff9f26f08c25352c3eaf9eb7ee
Grecha <mishavrnn@icloud.com>
2022-04-02 01:53:57 +0300

    дз 3 и правки дз 2

--- Js-homework_2/js/homework2.js
+++ Js-homework_2/js/homework2.js
@@ -2,19 +2,22 @@
 
 // //Задача на if----------
 
-let count = prompt('Количество верных ответов');
+let count = parseInt(prompt('Количество верных ответов'));
+    count = !isNaN(parseInt(count)) ? parseInt(count) : -1; // проверка на ошибочный ввод пользователем числа
 
-if (count <= 39 && count >= 0){ 
-    console.log('Попробуйте еще раз');
-} else if (count <= 59 && count >= 40){ 
-    console.log('Удовлетворительно');
-} else if (count <= 89 && count >= 60){
-    console.log('Хорошо');
-} else if (count <= 100 && count >= 90) {
-    console.log('Отлично');
-} else {
+if (count < 0 || count > 100){
     console.log('Условие не выполнено');
+} else if (count <= 39){ 
+    console.log('Попробуйте еще раз');
+} else if (count <= 59){ 
+    console.log('Удовлетворительно');
+} else if (count <= 89){
+    console.log('Хорошо');
+} else if (count <= 100) {
+    console.log('Отлично');
 }
+
+
 
 // //Задача на swift-------
 
@@ -44,9 +47,12 @@ switch (code) {
 let plates = parseInt(prompt('Введите колличество тарелок'));
 let fairy = parseInt(prompt('Введите колличество моющего средства'));
 
-while (true) {
+plates = !isNaN(parseInt(plates)) ? parseInt(plates) : -1; // проверка на ошибочный ввод пользователем числа
+fairy = !isNaN(parseInt(fairy)) ? parseInt(fairy) : -1; // проверка на ошибочный ввод пользователем числа
+while (plates > 0 && fairy > 0) { //задаем условие - выполнять пока есть тарелки >0 и есть моющее средство >0
    plates --; //Декремент (--) уменьшает значение переменной на 1.
    fairy -= 0.5; //Присваивание с вычитанием (x -= y) =>  x = x - y
+   console.log(`Моющего средства осталось: ${fairy}`);
     if (plates === 0) {
        console.log(`Тарелки закончились, а моющего средства осталось: ${fairy}`);
        break;
